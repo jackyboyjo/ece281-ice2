e4f8ad148cbf6803a60bf94cc8dce738864d5e3a
jackyboyjo <github noreply email@useres.noreply.github.com>
2024-02-01 22:07:04 -0700

    ICE2 hopefully working

--- src/hdl/halfAdder.vhd
+++ src/hdl/halfAdder.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : halfAdder.vhd
---| AUTHOR(S)     : Capt Warner
---| CREATED       : 01/17/2017
+--| AUTHOR(S)     : Cadet Isacco
+--| CREATED       : 01/25/2024
 --| DESCRIPTION   : This file implements a one bit half adder.
 --|
 --| DOCUMENTATION : None
@@ -23,7 +23,7 @@
 --|
 --|    Libraries : ieee
 --|    Packages  : std_logic_1164, numeric_std, unisim
---|    Files     : LIST ANY DEPENDENCIES
+--|    Files     : NONE
 --|
 --+----------------------------------------------------------------------------
 --|
@@ -56,20 +56,29 @@ entity halfAdder is
   port(
 	i_A     : in  std_logic; -- 1-bit input port
 	i_B     : in  std_logic; 
-	o_S     : out std_logic  -- 1-bit output port
-							 -- (NOTE: NO semicolon on LAST port only!)
+	o_S     : out std_logic; -- 1-bit output port
+	o_Cout  : out std_logic						 -- (NOTE: NO semicolon on LAST port only!)
 	-- TODO:  Carry port
   ); -- the semicolon is here instead
 end halfAdder;
 
 architecture halfAdder_arch of halfAdder is 
 	-- this is where you would include components declarations and signals if you needed them
-	  
+	  component halfAdder is
+      port(
+        i_A     : in  std_logic; -- 1-bit input port
+        i_B     : in  std_logic;
+        o_S     : out std_logic;  -- 1-bit output port
+        o_Cout  : out std_logic          -- (NOTE: NO semicolon on LAST port only!)
+      -- TODO:  Carry port
+      ); -- the semicolon is here instead
+      end component;
 begin
 	-- this is where you would map ports for any component instantiations if you needed to
 
 	-- *concurrent* signal assignments
 	o_S    <= i_A xor i_B;
+	o_Cout <= i_A and i_B;
 	-- TODO:  Carry signal assignment
 	
 end halfAdder_arch;
